1b4280e77ddb01aa5ed891dfd2c03401bb880718
Alan Anaya Araujo <alan@Alans-MacBook-Air.local>
2024-03-10 18:20:43 -0600

    fix: refactored verify code setState function for code and build issues fixed

--- src/components/modals/VerificationCode/verifyCode.jsx
+++ src/components/modals/VerificationCode/verifyCode.jsx
@@ -1,14 +1,14 @@
-import React, { Component } from 'react'
-import { LOGIN_LOCALE } from 'src/utils/constants'
-import { connect } from 'react-redux'
-import { Modal } from '@mui/material'
-import { formStyle, inputStyle, buttonStyle, modalContentStyle } from './styles'
-import { setInputPasswords } from 'src/store/users/reducer'
-import { validateVerificationCode } from 'src/store/users/actions'
-import { default as PasswordInputs } from './inputPasswords'
+import React, {Component} from 'react'
+import {LOGIN_LOCALE} from 'src/utils/constants'
+import {connect} from 'react-redux'
+import {Modal} from '@mui/material'
+import {formStyle, inputStyle, buttonStyle, modalContentStyle} from './styles'
+import {setInputPasswords} from 'src/store/users/reducer'
+import {validateVerificationCode} from 'src/store/users/actions'
+import {default as PasswordInputs} from './inputPasswords'
 import Button from '@mui/material/Button'
 
-const mapstatetoprops = (state) => ({
+const mapstatetoprops = state => ({
   showInputPasswords: state.users.showInputPasswords
 })
 
@@ -27,21 +27,21 @@ class VerifyCodeModal extends Component {
 
   onShowInputPasswords = async () => {
     let body = {
-      email: this.email,
-      code: Number(this.state.code.join(LOGIN_LOCALE.EMPTY_STRING))
-    },
-    response = await this.props.validateVerificationCode(body),
-    payload = response.payload;
+        email: this.email,
+        code: Number(this.state.code.join(LOGIN_LOCALE.EMPTY_STRING))
+      },
+      response = await this.props.validateVerificationCode(body),
+      payload = response.payload
 
-    if(payload === LOGIN_LOCALE.ERROR_CODE) {
+    if (payload === LOGIN_LOCALE.ERROR_CODE) {
       return
     } else {
       let hasFolio = payload.folio ? true : false,
-        message = payload.message;
+        message = payload.message
 
-        if(hasFolio && message.includes(LOGIN_LOCALE.VALID_CODE)) {
-          this.props.setInputPasswords(true);
-        }
+      if (hasFolio && message.includes(LOGIN_LOCALE.VALID_CODE)) {
+        this.props.setInputPasswords(true)
+      }
     }
   }
 
@@ -49,11 +49,17 @@ class VerifyCodeModal extends Component {
     let inputIndex = parseInt(window.event.target.dataset.index),
       number = window.event.target.value,
       currentIndexInput = inputIndex
-    this.state.code[inputIndex] = number
+
+    const updatedCode = [...this.state.code]
+    updatedCode[inputIndex] = number
+    this.setState({code: updatedCode})
 
     if (inputIndex >= 0 && inputIndex <= 4 && number !== LOGIN_LOCALE.EMPTY_STRING) {
       currentIndexInput = inputIndex += 1
-    } else if ((inputIndex === 5 && number !== LOGIN_LOCALE.EMPTY_STRING) || (inputIndex === 0 && number === LOGIN_LOCALE.EMPTY_STRING)) {
+    } else if (
+      (inputIndex === 5 && number !== LOGIN_LOCALE.EMPTY_STRING) ||
+      (inputIndex === 0 && number === LOGIN_LOCALE.EMPTY_STRING)
+    ) {
       currentIndexInput = inputIndex
     } else {
       currentIndexInput = inputIndex -= 1
@@ -70,55 +76,55 @@ class VerifyCodeModal extends Component {
     return (
       <Modal open={this.open} onClose={this.handleClose}>
         <div style={modalContentStyle}>
-          { !this.props.showInputPasswords &&
+          {!this.props.showInputPasswords && (
             <div className='inputs--container'>
-            <h2 class='heading-secondary margin-bottom-m'>{LOGIN_LOCALE.ENTER_CODE}</h2>
-            <p class='margin-bottom-s'>{LOGIN_LOCALE.ENTER_CODE_RECOVERY_PASSWORD} </p>
-            <form class='form--verifiy-code' style={formStyle}>
-              {this.state.code.map((item, idx) => {
-                return (
-                  <input
-                    key={idx}
-                    id={idx}
-                    name={'input' + idx}
-                    ref={this.nameInput}
-                    class='input input--single-char'
-                    placeholder='0'
-                    size='1'
-                    maxlength='1'
-                    style={inputStyle}
-                    onChange={e => {
-                      this.handleForm.call(this, e)
-                    }}
-                    onKeyPress={(event) => {
-                      if (!/[0-9]/.test(event.key)) {
-                        event.preventDefault();
-                      }
-                    }}
-                    data-index={idx}
-                  />
-                )
-              })}
-              <Button style={buttonStyle} onClick={this.onShowInputPasswords}>&rarr;</Button>
-            </form>
+              <h2 className='heading-secondary margin-bottom-m'>{LOGIN_LOCALE.ENTER_CODE}</h2>
+              <p className='margin-bottom-s'>{LOGIN_LOCALE.ENTER_CODE_RECOVERY_PASSWORD} </p>
+              <form className='form--verifiy-code' style={formStyle}>
+                {this.state.code.map((item, idx) => {
+                  return (
+                    <input
+                      key={idx}
+                      id={idx}
+                      name={'input' + idx}
+                      ref={this.nameInput}
+                      className='input input--single-char'
+                      placeholder='0'
+                      size='1'
+                      maxLength='1'
+                      style={inputStyle}
+                      onChange={e => {
+                        this.handleForm.call(this, e)
+                      }}
+                      onKeyPress={event => {
+                        if (!/[0-9]/.test(event.key)) {
+                          event.preventDefault()
+                        }
+                      }}
+                      data-index={idx}
+                    />
+                  )
+                })}
+                <Button style={buttonStyle} onClick={this.onShowInputPasswords}>
+                  &rarr;
+                </Button>
+              </form>
             </div>
-          }
-          {
-            this.props.showInputPasswords && (
-              <PasswordInputs email={this.email} code={Number(this.state.code.join(LOGIN_LOCALE.EMPTY_STRING))}  />
-            )
-          }
+          )}
+          {this.props.showInputPasswords && (
+            <PasswordInputs email={this.email} code={Number(this.state.code.join(LOGIN_LOCALE.EMPTY_STRING))} />
+          )}
         </div>
       </Modal>
     )
   }
 }
 
-const mapdispatchtoprops = (dispatch) => {
+const mapdispatchtoprops = dispatch => {
   return {
-    setInputPasswords: (value) => dispatch(setInputPasswords(value)),
-    validateVerificationCode: (body) => dispatch(validateVerificationCode(body))
+    setInputPasswords: value => dispatch(setInputPasswords(value)),
+    validateVerificationCode: body => dispatch(validateVerificationCode(body))
   }
-};
+}
 
 export default connect(mapstatetoprops, mapdispatchtoprops)(VerifyCodeModal)
--- src/pages/forgot-password/passwordIlustration.jsx
+++ src/pages/forgot-password/passwordIlustration.jsx
@@ -1,25 +1,23 @@
 import Box from '@mui/material/Box'
-import { ForgotPasswordIllustrationWrapper, 
-    ForgotPasswordIllustration
-} from './styles'
-import { useTheme } from '@mui/material/styles'
+import {ForgotPasswordIllustrationWrapper, ForgotPasswordIllustration} from '../../utils/styles'
+import {useTheme} from '@mui/material/styles'
 import FooterIllustrationsV2 from 'src/views/pages/auth/FooterIllustrationsV2'
 
-const PasswordIlustration = (props) => {
-    const theme = useTheme(),
-        imageSource = props.imageSource;
+const PasswordIlustration = props => {
+  const theme = useTheme(),
+    imageSource = props.imageSource
 
-    return (
-        <Box sx={{ flex: 1, display: 'flex', position: 'relative', alignItems: 'center', justifyContent: 'center' }}>
-            <ForgotPasswordIllustrationWrapper>
-                <ForgotPasswordIllustration
-                    alt='forgot-password-illustration'
-                    src={`/images/pages/${imageSource}-${theme.palette.mode}.png`}
-                    />
-                </ForgotPasswordIllustrationWrapper>
-            <FooterIllustrationsV2 />
-        </Box>
-    )
+  return (
+    <Box sx={{flex: 1, display: 'flex', position: 'relative', alignItems: 'center', justifyContent: 'center'}}>
+      <ForgotPasswordIllustrationWrapper>
+        <ForgotPasswordIllustration
+          alt='forgot-password-illustration'
+          src={`/images/pages/${imageSource}-${theme.palette.mode}.png`}
+        />
+      </ForgotPasswordIllustrationWrapper>
+      <FooterIllustrationsV2 />
+    </Box>
+  )
 }
 
 export default PasswordIlustration
