4d687a598d13f5a7d9661bdd490f441a1da36052
Diego Ponce <58534750+TheDMPV@users.noreply.github.com>
2024-05-14 10:11:19 -0600

    update users

--- src/pages/users/index.js
+++ src/pages/users/index.js
@@ -1,35 +1,126 @@
-import {Fragment} from 'react'
-import {useForm} from 'react-hook-form'
-import {useSelector, useDispatch} from 'react-redux'
+import {Simple} from 'src/components/simple'
+import {USERS_ENDPOINT} from 'src/services/endpoints'
 
-import {toggleDeleteModal} from 'src/store/users/reducer'
-import {deleteUser} from 'src/store/users/actions'
-import DataTable from './DataTable'
-import EditItem from './EditItem'
-import RemoveItem from 'src/views/modals/RemoveItem'
+export default function Users() {
+  const columns = [
+    {
+      flex: 0.25,
+      minWidth: 200,
+      field: 'firstname',
+      headerName: 'Nombre(s)'
+    },
+    {
+      flex: 0.25,
+      minWidth: 200,
+      field: 'lastname',
+      headerName: 'Apellidos'
+    },
+    {
+      flex: 0.25,
+      minWidth: 200,
+      field: 'email',
+      headerName: 'Correo electrónico'
+    },
+    {
+      flex: 0.25,
+      minWidth: 200,
+      field: 'phone',
+      headerName: 'Teléfono'
+    },
+    {
+      flex: 0.25,
+      minWidth: 200,
+      field: 'zoneID',
+      headerName: 'Zona'
+    },
+    {
+      flex: 0.25,
+      minWidth: 200,
+      field: 'position',
+      headerName: 'Posición'
+    }
+  ]
+  const form = [
+    {
+      label: 'Nombre(s)',
+      name: 'firstname',
+      type: 'text',
+      value: '',
+      isRequired: true,
+      width: 12
+    },
+    {
+      label: 'Apellidos',
+      name: 'lastname',
+      type: 'text',
+      value: '',
+      isRequired: true,
+      width: 12
+    },
+    {
+      label: 'Correo electrónico',
+      name: 'email',
+      type: 'text',
+      value: '',
+      isRequired: true,
+      width: 12
+    },
+    {
+      label: 'Teléfono',
+      name: 'phone',
+      type: 'text',
+      value: '',
+      isRequired: true,
+      width: 12
+    },
+    {
+      label: 'Contraseña',
+      name: 'password',
+      type: 'text',
+      value: '',
+      isRequired: true,
+      width: 12
+    },
+    {
+      label: 'Zona',
+      name: 'zoneID',
+      type: 'text',
+      value: '',
+      isRequired: true,
+      width: 12
+    },
 
-const Users = () => {
-  const dispatch = useDispatch(),
-    dialogText = 'Seguro que desea Eliminar usuario?',
-    methods = useForm(),
-    {isOpen, isDeleteOpen} = useSelector(state => state.users)
+    {
+      label: 'Posición',
+      name: 'position',
+      type: 'text',
+      value: '',
+      isRequired: true,
+      width: 12
+    }
+  ]
 
   return (
-    <Fragment>
-      <DataTable />
-      {isOpen && <EditItem methods={methods} isOpen={isOpen} />}
-
-      {isDeleteOpen && (
-        <RemoveItem
-          isOpen={isDeleteOpen}
-          deleteGeneric={deleteUser}
-          dialogText={dialogText}
-          toggleDeleteModal={toggleDeleteModal}
-          getRowFunction={state => state.users}
-        />
-      )}
-    </Fragment>
+    <Simple
+      table={{
+        label: 'Usuarios',
+        endpoints: {
+          baseUrl: `${USERS_ENDPOINT}/users`
+        },
+        loading: true,
+        showAddButton: true,
+        columns: columns,
+        actions: ['edit', 'detail', 'delete']
+      }}
+      modal={{
+        title: 'Crear usuario',
+        size: 'md',
+        form: form,
+        actions: {
+          back: 'Regresar',
+          save: 'Guardar'
+        }
+      }}
+    />
   )
 }
-
-export default Users
