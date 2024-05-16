534f11db1d209324887f96e782d6928571c4d7c0
Diego Ponce <58534750+TheDMPV@users.noreply.github.com>
2024-05-16 01:17:08 -0600

    Update index.js

--- src/components/simple/form/index.js
+++ src/components/simple/form/index.js
@@ -1,22 +1,24 @@
 import React from 'react'
 import {Grid, FormControl} from '@mui/material'
 import {Controller} from 'react-hook-form'
-import Text from './inputs/Text'
-import SelectField from './inputs/SelectField'
-
+import Text from 'src/components/simple/form/inputs/Text'
+import SelectField from 'src/components/simple/form/inputs/SelectField'
+import PasswordField from 'src/components/simple/form/inputs/PasswordField'
 const Inputs = ({input, value, onChange, error}) => {
   switch (input.type) {
     case 'text':
       return <Text input={input} value={value} onChange={onChange} error={error} />
     case 'select':
       return <SelectField input={input} value={value} onChange={onChange} error={error} />
+    case 'password':
+      return <PasswordField input={input} value={value} onChange={onChange} error={error} />
     default:
       return <Text input={input} value={value} onChange={onChange} error={error} />
   }
 }
 
 const Form = ({inputs, control, resetField, reset, setValue, getValues}) => {
-  const filteredInputs = inputs.filter(input => !input.onlyTable)
+  const filteredInputs = inputs.filter(input => !input.hideInput)
 
   return (
     <Grid sx={{pt: '1rem'}} container spacing={5}>
