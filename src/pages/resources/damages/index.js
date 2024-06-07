a71c2dfd3938eb2d9d55c913e863bd60307a6b4f
Diego Ponce <58534750+TheDMPV@users.noreply.github.com>
2024-06-07 01:54:02 -0600

    Siniestros

--- src/pages/resources/damages/index.js
+++ src/pages/resources/damages/index.js
@@ -1,35 +1,74 @@
-import {Fragment} from 'react'
-import {useForm} from 'react-hook-form'
-import {useSelector, useDispatch} from 'react-redux'
+// import {Fragment} from 'react'
+// import {useForm} from 'react-hook-form'
+// import {useSelector, useDispatch} from 'react-redux'
 
-import {toggleDeleteModal} from 'src/store/catalogs/damages/reducer'
-import {deleteDamageCat} from 'src/store/catalogs/damages/actions'
-import DataTable from 'src/pages/resources/damages/DataTable'
-import EditItem from 'src/pages/resources/damages/EditItem'
-import RemoveItem from 'src/views/modals/RemoveItem'
+// import {toggleDeleteModal} from 'src/store/catalogs/damages/reducer'
+// import {deleteDamageCat} from 'src/store/catalogs/damages/actions'
+// import DataTable from 'src/pages/resources/damages/DataTable'
+// import EditItem from 'src/pages/resources/damages/EditItem'
+// import RemoveItem from 'src/views/modals/RemoveItem'
 
-const Damages = () => {
-  const dispatch = useDispatch(),
-    dialogText = 'Seguro de eliminar el siniestro seleccionado?',
-    methods = useForm(),
-    {isOpen, isDeleteOpen} = useSelector(state => state.damagesCat)
+// const Damages = () => {
+//   const dispatch = useDispatch(),
+//     dialogText = 'Seguro de eliminar el siniestro seleccionado?',
+//     methods = useForm(),
+//     {isOpen, isDeleteOpen} = useSelector(state => state.damagesCat)
+
+//   return (
+//     <Fragment>
+//       <DataTable methods={methods} />
+//       {isOpen && <EditItem methods={methods} isOpen={isOpen} />}
+
+//       {isDeleteOpen && (
+//         <RemoveItem
+//           isOpen={isDeleteOpen}
+//           deleteGeneric={deleteDamageCat}
+//           dialogText={dialogText}
+//           toggleDeleteModal={toggleDeleteModal}
+//           getRowFunction={state => state.damagesCat}
+//         />
+//       )}
+//     </Fragment>
+//   )
+// }
+
+// export default Damages
+
+import {Simple} from 'src/components/simple'
+import {DAMAGES_CAT_ENDPOINT} from 'src/services/endpoints'
+export default function DimensionsCat() {
+  const data = [
+    {
+      headerName: 'Siniestro',
+      field: 'name',
+      type: 'text',
+      value: '',
+      isRequired: true,
+      width: 12,
+      flex: true
+    }
+  ]
 
   return (
-    <Fragment>
-      <DataTable methods={methods} />
-      {isOpen && <EditItem methods={methods} isOpen={isOpen} />}
-
-      {isDeleteOpen && (
-        <RemoveItem
-          isOpen={isDeleteOpen}
-          deleteGeneric={deleteDamageCat}
-          dialogText={dialogText}
-          toggleDeleteModal={toggleDeleteModal}
-          getRowFunction={state => state.damagesCat}
-        />
-      )}
-    </Fragment>
+    <Simple
+      table={{
+        label: 'Siniestros',
+        endpoints: {
+          baseUrl: `${DAMAGES_CAT_ENDPOINT}/damages-cat`
+        },
+        columns: data,
+        showAddButton: true,
+        actions: ['edit', 'delete']
+      }}
+      modal={{
+        title: 'Crear Siniestro',
+        form: data,
+        size: 'sm',
+        actions: {
+          back: 'Regresar',
+          save: 'Guardar'
+        }
+      }}
+    />
   )
 }
-
-export default Damages
